f9a71d2ca324a77952018405a87076a81b190442
Adam Rizk <adamrizk9@gmail.com>
2022-10-23 03:24:29 +0530

    Added logs endpoint

--- src/main/java/com/quantumtrader/controller/QuantumTraderController.java
+++ src/main/java/com/quantumtrader/controller/QuantumTraderController.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quantumtrader.entity.Stock;
@@ -20,11 +22,24 @@ public class QuantumTraderController {
         return id*2;
     }
 
-    @GetMapping(value = "/stock/{id}", produces = "application/json")
-    public ResponseEntity<Stock> returnStock(@PathVariable int id) {
-        if (id == 3) {
+    @GetMapping(value = "/user/{id}", produces = "application/json")
+    public ResponseEntity<Stock> returnStock(@PathVariable int id, @RequestParam String stockname) {
+        stockname = stockname.toLowerCase();
+        if (id == 3 && stockname.equals("bby")) {
             System.out.println("Returning the following:" + stock.toString());
             return new ResponseEntity<>(stock, HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @ResponseBody
+    @GetMapping("/user/{id}/logs")
+    public ResponseEntity<String> returnLogs(@PathVariable int id) {
+        if (id == 3) {
+            System.out.println("Returning the following:" + stock.toString());
+            return new ResponseEntity<>(stock.toString(), HttpStatus.OK);
         }
         else {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
