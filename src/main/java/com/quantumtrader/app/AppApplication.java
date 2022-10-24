eca6a64abf35ff93bf44ca904f5d08e6a70f2a1b
Adam Rizk <adamrizk9@gmail.com>
2022-10-25 05:27:49 +0530

    Fixed DB bug

--- src/main/java/com/quantumtrader/app/AppApplication.java
+++ src/main/java/com/quantumtrader/app/AppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.quantumtrader.controller", "com.quantumtrader.service"})
+@ComponentScan(basePackages = {"com.quantumtrader.controller", "com.quantumtrader.service", "com.quantumtrader.entity"})
 public class AppApplication {
 
 	public static void main(String[] args) {
