b8a642363327f62f50c8d233ffc4735d2798c712
Adam Rizk <adamrizk9@gmail.com>
2022-10-25 06:02:21 +0530

    Added EntityScan

--- src/main/java/com/quantumtrader/app/AppApplication.java
+++ src/main/java/com/quantumtrader/app/AppApplication.java
@@ -2,10 +2,12 @@ package com.quantumtrader.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.quantumtrader.controller", "com.quantumtrader.service", "com.quantumtrader.entity"})
+@ComponentScan(basePackages = {"com.quantumtrader.controller", "com.quantumtrader.service"})
+@EntityScan("com.quantumtrader.entity")
 public class AppApplication {
 
 	public static void main(String[] args) {
